be7784dbc5a1c80673365330f5e8e0e7536c863c
Nishtha Grover <46939668+nishthagrover@users.noreply.github.com>
2020-06-03 16:46:35 +0530

    Create Day_1_Data_Types.py
    link to the complete problem statement: https://www.hackerrank.com/challenges/30-data-types/problem

---
+++ Day_1_Data_Types.py
@@ -0,0 +1,18 @@
+#link to the complete problem statement: https://www.hackerrank.com/challenges/30-data-types/problem
+#Solution
+
+
+#given values
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+#taking input
+j = int (input())
+c = float(input())
+t = input()
+
+#print the required output
+print(i+j)
+print(d+c)
+print(s+t)
