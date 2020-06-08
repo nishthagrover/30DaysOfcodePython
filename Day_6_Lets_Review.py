fc154f52917072fd1fa3c6e8f3a2b2bab0731da4
Nishtha Grover <46939668+nishthagrover@users.noreply.github.com>
2020-06-09 00:37:53 +0530

    Day 6: Let's Review
    Link to the complete Problem Statement: https://www.hackerrank.com/challenges/30-review-loop/problem

--- Day_6_Lets_Review.py
+++ Day_6_Lets_Review.py
@@ -1,1 +1,22 @@
 # Link to the complete Problem Statement: https://www.hackerrank.com/challenges/30-review-loop/problem
+
+# Solution
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+
+    test = int(input())
+    for i in range(test):
+        s = input()
+        
+        for i in range (len(s)):
+            if i%2 == 0:
+                print(s[i],sep = "", end = "")
+        
+        print(" ",sep = "", end = "")
+
+        for i in range (len(s)):
+            if i%2 == 1:
+                print(s[i],sep = "", end = "")
+        
+        print("")
