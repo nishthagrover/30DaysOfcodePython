7d036179ea9f21ca7794403d7d362142d3559184
Nishtha Grover <46939668+nishthagrover@users.noreply.github.com>
2020-06-16 14:26:18 +0530

    Day 13: Abstract Classes
    Link to the complete problem statement: https://www.hackerrank.com/challenges/30-abstract-classes/problem

--- Day_13_Abstract_Classes.py
+++ Day_13_Abstract_Classes.py
@@ -1,2 +1,27 @@
 # Link to the complete problem statement: https://www.hackerrank.com/challenges/30-abstract-classes/problem
 # solution
+
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self,title,author,price):
+        Book.__init__(self,title,author)
+        self.price = price
+    
+    def display(self):
+        print("Title: "+ title)
+        print("Author: "+ author)
+        print("Price: "+ str(price))
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
