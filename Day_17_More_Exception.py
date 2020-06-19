c93c7657bf4ee88d1ec04667a849ddd155e79aa2
Nishtha Grover <46939668+nishthagrover@users.noreply.github.com>
2020-06-19 23:44:58 +0530

    Day 17: More Exceptions
    Link to the complete problem statement: hackerrank.com/challenges/30-more-exceptions/problem

---
+++ Day_17_More_Exception.py
@@ -0,0 +1,19 @@
+# Link to the complete problem statement: hackerrank.com/challenges/30-more-exceptions/problem
+# Solution
+
+class Calculator():
+    def power(self, n, p):
+        if n < 0 or p < 0:
+            raise RuntimeError("n and p should be non-negative")
+        
+        return pow(n, p)
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
